9681ab97b1296d25e7270b65dddbb764e4867a3c
Ion Dormenco <idormenco@gmail.com>
2020-03-26 10:54:51 +0200

    improve furthermore logging

--- backend/Code4Ro.CoViz19.Api/Program.cs
+++ backend/Code4Ro.CoViz19.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Code4Ro.CoViz19.Api
 {
@@ -12,6 +13,12 @@ namespace Code4Ro.CoViz19.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddDebug();
+                    logging.AddConsole();
+                    logging.SetMinimumLevel(LogLevel.None);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- backend/Code4Ro.CoViz19.Services/HttpFileService.cs
+++ backend/Code4Ro.CoViz19.Services/HttpFileService.cs
@@ -2,18 +2,27 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Code4Ro.CoViz19.Services.Options;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Code4Ro.CoViz19.Services
 {
     public class HttpFileService : IFileService
     {
+        private readonly ILogger _logger;
         private readonly HttpFileServiceOptions _options;
-        public HttpFileService(IOptions<HttpFileServiceOptions> options)
+        public HttpFileService(IOptions<HttpFileServiceOptions> options, ILogger<HttpFileService> logger)
         {
+            _logger = logger;
             _options = options.Value;
         }
-        public string GetRawData() => new HttpClient().GetAsync(_options.JsonFileUrl).Result.Content.ReadAsStringAsync().Result;
+
+        public string GetRawData()
+        {
+            _logger.LogInformation($"will fetch file from {_options.JsonFileUrl}");
+            var result = new HttpClient().GetAsync(_options.JsonFileUrl).Result.Content.ReadAsStringAsync().Result;
+            return result;
+        }
 
 #pragma warning disable 1998
         public async Task SaveRawData(string fileContent) => throw new NotImplementedException();
